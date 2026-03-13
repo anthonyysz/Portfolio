0bb00ead590ecdf09abeea0ae3d3de3a49e80098
anthonyysz <anthony_szabo@hotmail.com>
2026-03-13 11:30:04 -0400

    Added contact.css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import '../style/Contact.css'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#081849] flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/ca2a461a-5f79-45f2-a393-86ad800a3e97" className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-6 sm:text-center'>
-                <p className='text-4xl font-bold inline border-b-4 border-[#C4A484] text-[#F4F0C6]'>Contact</p>
-                <p className='text-[#F4F0C6] py-4'>Submit the form below or send me an email at anthony_szabo@hotmail.com</p>
+    <div name='contact' className='contact-screen'>
+        <form method='POST' action="https://getform.io/f/ca2a461a-5f79-45f2-a393-86ad800a3e97" className='contact-form'>
+            <div className='contact-titlebox'>
+                <p className='contact-title'>Contact</p>
+                <p className='contact-subtitle'>Submit the form below or send me an email at anthony_szabo@hotmail.com</p>
             </div>
-            <input className='bg-gray-300 p-2' type="text" placeholder='Name' name='name'></input>
-            <input className='my-4 p-2 bg-gray-300' type="email" placeholder='Email' name='email'></input>
-            <textarea className='p-2 bg-gray-300' name='message' rows='10' placeholder='Message'></textarea>
-            <button className='text-[#F4F0C6] border-2 hover:bg-[#C4A484] hover:border-[#C4A484] px-4 py-3 my-8 mx-auto flex items-center'>Let's Talk!</button>
+            <input className='contact-name-input' type="text" placeholder='Name' name='name'></input>
+            <input className='contact-email-input' type="email" placeholder='Email' name='email'></input>
+            <textarea className='contact-message-input' name='message' rows='10' placeholder='Message'></textarea>
+            <button className='form-submit'>Let's Talk!</button>
         </form>
     </div>
   )
