cc1ad5a66c10974cb04a5d8eb0bb1a45b9c3173a
anthonyysz <anthony_szabo@hotmail.com>
2026-03-13 01:02:56 -0400

    Added home.css, changed site icon

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
 import {Link} from 'react-scroll'
+import '../style/Home.css'
+
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#081849]'>
-
-        {/*Container*/}
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className='text-l text-[#C4A484]'>Hi, my name is</p>
-            <h1 className='text-4xl sm:text-7xl font-bold text-[#F8F0C6]'>Anthony Szabo</h1>
-            <h2 className='text-4xl font-bold text-[#F8F0C6]'>I'm a Data Scientist</h2>
-            <p className='text-l text-[#F8F0C6] py-4 max-w-[750px] text-[#F8F0C6]'>I'm a data scientist with a deep and expansive knowledge of statistics and I love digging into 
+    <div name='home' className='home-screen'>
+        <div className='container'>
+            <p className='greeting-text'>Hi, my name is</p>
+            <h1 className='name-text'>Anthony Szabo</h1>
+            <h2 className='position-text'>I'm a Data Scientist</h2>
+            <p className='bio-text'>I'm a data scientist with a deep and expansive knowledge of statistics and I love digging into 
                 projects within that realm. Currently I am working to expand my knowledge of machine learning, especially in neural networks, 
                 while finishing my Bachelor's degree in Computer Science from Western Governor's University.</p>
             <div>
-                <button className='text-[#F8F0C6] group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#C4A484] hover:border-[#C4A484]'>
-                  <Link to="about" smooth={true} duration={500}>
+                <a href='/work'>
+                  <button className='view-work-button'>
                     View Work
-                  </Link>
-                  <span className='group-hover:rotate-90 duration-300'>
-                    <HiArrowNarrowRight className='ml-3' />
-                  </span>
-                </button>
+                    <span className='arrow'>
+                      <HiArrowNarrowRight className='' />
+                    </span>
+                  </button>
+                </a>
             </div>
         </div>
     </div>
