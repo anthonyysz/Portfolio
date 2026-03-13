bb843a4093d371af50b780ef0fcead3382c7f18a
anthonyysz <anthony_szabo@hotmail.com>
2026-03-12 23:16:00 -0400

    Added css file to skills and work pages

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,43 +5,42 @@ import Python from '../assets/python.png'
 import ReactImage from '../assets/react.png'
 import SQL from '../assets/sql.png'
 import Tailwind from '../assets/tailwind.png'
+import '../style/Skills.css'
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen bg-[#081849] text-[#F8F0C6]'>
-      <div className='max-w-[1000px] mx-auto p-4 pl-8 flex flex-col justify-center w-full h-full'>
-       <div className='sm:text-center'>
-         <p className='text-4xl font-bold inline border-b-4 border-[#C4A484]'>Skills</p>
-         <p className='pt-4'>These are some of the software that I've worked with:</p>
+    <div name='skills' className='skills-screen'>
+       <div className='skills-titlebox'>
+         <p className='skills-title'>Skills</p>
+         <p className='skills-subtitle'>These are some of the software that I've worked with:</p>
         </div>
          
-       <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-6'>
-         <div className='bg-[#7a6249] shadow-md shadow-[#040C16] hover:scale-105 duration-500'>
-            <img className='w-20 mx-auto py-2' src={GitHub} alt="GitHub icon" />
-            <p className='pt-1'>GitHub</p>
+       <div className='skills-grid'>
+         <div className='skills-grid-item hover:scale-105 duration-500'>
+            <img className='skills-image' src={GitHub} alt="GitHub icon" />
+            <p className='skills-image-label'>GitHub</p>
           </div>
-          <div className='bg-[#7a6249] shadow-md shadow-[#040C16] hover:scale-105 duration-500'>
-            <img className='w-20 mx-auto py-2' src={SQL} alt="SQL icon" />
-            <p className='pt-1'>SQL</p>
+          <div className='skills-grid-item hover:scale-105 duration-500'>
+            <img className='skills-image' src={SQL} alt="SQL icon" />
+            <p className='skills-image-label'>SQL</p>
           </div>
-          <div className='bg-[#7a6249] shadow-md shadow-[#040C16] hover:scale-105 duration-500'>
-            <img className='w-20 mx-auto py-2' src={Python} alt="Python icon" />
-            <p className='pt-1'>Python</p>
+          <div className='skills-grid-item hover:scale-105 duration-500'>
+            <img className='skills-image' src={Python} alt="Python icon" />
+            <p className='skills-image-label'>Python</p>
           </div>
-          <div className='bg-[#7a6249] shadow-md shadow-[#040C16] hover:scale-105 duration-500'>
-            <img className='w-20 mx-auto py-2' src={Pandas} alt="Pandas icon" />
-            <p className='pt-1'>Pandas</p>
+          <div className='skills-grid-item hover:scale-105 duration-500'>
+            <img className='skills-image' src={Pandas} alt="Pandas icon" />
+            <p className='skills-image-label'>Pandas</p>
           </div>
-          <div className='bg-[#7a6249] shadow-md shadow-[#040C16] hover:scale-105 duration-500'>
-            <img className='w-20 mx-auto py-2' src={ReactImage} alt="React Icon" />
-            <p className='pt-1'>React</p>
+          <div className='skills-grid-item hover:scale-105 duration-500'>
+            <img className='skills-image' src={ReactImage} alt="React Icon" />
+            <p className='skills-image-label'>React</p>
           </div>
-          <div className='bg-[#7a6249] shadow-md shadow-[#040C16] hover:scale-105 duration-500'>
-            <img className='w-20 mx-auto py-2' src={Tailwind} alt="Tailwind icon" />
-            <p className='pt-1'>Tailwind</p>
+          <div className='skills-grid-item hover:scale-105 duration-500'>
+            <img className='skills-image' src={Tailwind} alt="Tailwind icon" />
+            <p className='skills-image-label'>Tailwind</p>
           </div>
        </div>
-      </div>
     </div>
   )
 }
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,56 +3,53 @@ import '../style/Work.css'
 const Work = () => {
   return (
     <div name='work' className='work-screen'>
-        <div className='work-screen'>
-            <div className='work-titlebox'>
-                <p className='work-title'>Work</p>
-                <p className='work-subtitle'>Here's some of my recent work:</p>
+        <div className='work-titlebox'>
+            <p className='work-title'>Work</p>
+            <p className='work-subtitle'>Here's some of my recent work:</p>
+        </div>
+
+        <div className='work-grid'>
+            <div className='work-grid-item hover:scale-105 duration-200'>
+                <div className='item-top'>
+                    Portfolio Website
+                </div>
+                <div className='item-bottom'>
+                    <a href="/">
+                        <button className='button'>Demo</button>
+                    </a>
+                    <a href="https://github.com/anthonyysz/portfolio">
+                        <button className='button'>Code</button>
+                    </a>
+                </div>
             </div>
-
-            <div className='work-grid'>
-                <div className='grid-item hover:scale-105 duration-200'>
-                    <div className='item-top'>
-                        Portfolio Website
-                    </div>
-                    <div className='item-bottom'>
-                        <a href="/">
-                            <button className='button'>Demo</button>
-                        </a>
-                        <a href="https://github.com/anthonyysz/portfolio">
-                            <button className='button'>Code</button>
-                        </a>
-                    </div>
+            <div className='work-grid-item hover:scale-105 duration-200'>
+                <div className='item-top'>
+                    Time Series Analysis
                 </div>
-                <div className='grid-item hover:scale-105 duration-200'>
-                    <div className='item-top'>
-                        Time Series Analysis
-                    </div>
-                    <div className='item-bottom'>
-                        <a href="https://github.com/anthonyysz/CapstoneThree">
-                            <button className='button'>Code</button>
-                        </a>
-                    </div>
+                <div className='item-bottom'>
+                    <a href="https://github.com/anthonyysz/CapstoneThree">
+                        <button className='button'>Code</button>
+                    </a>
                 </div>
-                <div className='grid-item hover:scale-105 duration-200'>
-                    <div className='item-top'>
-                        Fruit Image Recognition
-                    </div>
-                    <div className='item-bottom'>
-                        <a href="https://github.com/anthonyysz/fruit_image_recognition">
-                            <button className='button'>Code</button>
-                        </a>
-                    </div>
+            </div>
+            <div className='work-grid-item hover:scale-105 duration-200'>
+                <div className='item-top'>
+                    Fruit Image Recognition
                 </div>
-
-                <div className='grid-item hover:scale-105 duration-200'>
-                    <div className='item-top'>
-                        Restaurant Reviews NLP
-                    </div>
-                    <div className='item-bottom'>
-                        <a href="https://github.com/anthonyysz/restaurant_reviews_nlp">
-                            <button className='button'>Code</button>
-                        </a>
-                    </div>
+                <div className='item-bottom'>
+                    <a href="https://github.com/anthonyysz/fruit_image_recognition">
+                        <button className='button'>Code</button>
+                    </a>
+                </div>
+            </div>
+            <div className='work-grid-item hover:scale-105 duration-200'>
+                <div className='item-top'>
+                    Restaurant Reviews NLP
+                </div>
+                <div className='item-bottom'>
+                    <a href="https://github.com/anthonyysz/restaurant_reviews_nlp">
+                        <button className='button'>Code</button>
+                    </a>
                 </div>
             </div>
         </div>
