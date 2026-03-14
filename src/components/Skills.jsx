d1d466c96ea1edb82316fd8be246d5062ce5b635
anthonyysz <anthony_szabo@hotmail.com>
2026-03-13 23:15:17 -0400

    Updated my skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,6 +4,11 @@ import Python from '../assets/python.png'
 import ReactImage from '../assets/react.png'
 import SQL from '../assets/sql.png'
 import Tailwind from '../assets/tailwind.png'
+import CPP from '../assets/cpp.png'
+import Java from '../assets/java.png'
+import Jupyter from '../assets/jupyter.png'
+import Lua from '../assets/lua.png'
+import Spring from '../assets/spring.png'
 import '../style/Skills.css'
 
 const Skills = () => {
@@ -15,17 +20,25 @@ const Skills = () => {
         </div>
          
        <div className='skills-grid'>
-         <div className='skills-grid-item hover:scale-105 duration-500'>
-            <img className='skills-image' src={GitHub} alt="GitHub icon" />
-            <p className='skills-image-label'>GitHub</p>
+          <div className='skills-grid-item hover:scale-105 duration-500'>
+            <img className='skills-image' src={Python} alt="Python icon" />
+            <p className='skills-image-label'>Python</p>
+          </div>
+          <div className='skills-grid-item hover:scale-105 duration-500'>
+            <img className='skills-image' src={Java} alt="Java icon" />
+            <p className='skills-image-label'>Java</p>
+          </div>
+          <div className='skills-grid-item hover:scale-105 duration-500'>
+            <img className='skills-image' src={CPP} alt="C++ icon" />
+            <p className='skills-image-label'>C++</p>
+          </div>
+          <div className='skills-grid-item hover:scale-105 duration-500'>
+            <img className='skills-image' src={Lua} alt="Lua icon" />
+            <p className='skills-image-label'>Lua</p>
           </div>
           <div className='skills-grid-item hover:scale-105 duration-500'>
             <img className='skills-image' src={SQL} alt="SQL icon" />
             <p className='skills-image-label'>SQL</p>
-          </div>
-          <div className='skills-grid-item hover:scale-105 duration-500'>
-            <img className='skills-image' src={Python} alt="Python icon" />
-            <p className='skills-image-label'>Python</p>
           </div>
           <div className='skills-grid-item hover:scale-105 duration-500'>
             <img className='skills-image' src={Pandas} alt="Pandas icon" />
@@ -38,6 +51,18 @@ const Skills = () => {
           <div className='skills-grid-item hover:scale-105 duration-500'>
             <img className='skills-image' src={Tailwind} alt="Tailwind icon" />
             <p className='skills-image-label'>Tailwind</p>
+          </div>
+          <div className='skills-grid-item hover:scale-105 duration-500'>
+            <img className='skills-image' src={Jupyter} alt="Jupyter icon" />
+            <p className='skills-image-label'>Jupyter</p>
+          </div>
+          <div className='skills-grid-item hover:scale-105 duration-500'>
+            <img className='skills-image' src={Spring} alt="Spring icon" />
+            <p className='skills-image-label'>Spring</p>
+          </div>
+         <div className='skills-grid-item hover:scale-105 duration-500'>
+            <img className='skills-image' src={GitHub} alt="GitHub icon" />
+            <p className='skills-image-label'>GitHub</p>
           </div>
        </div>
     </div>
